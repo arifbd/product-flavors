1bbdfff83f51f6fe260fcdc67725150f572c5811
Md Arif <arif991846@gmail.com>
2019-04-27 13:32:00 +0600

    init commit

---
+++ app/src/free/java/utils/Calculator.kt
@@ -0,0 +1,5 @@
+object Calculator {
+    fun getNumberString(): String {
+        return "01YYYYYYYYY"
+    }
+}
---
+++ app/src/paid/java/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.ennoblesoft.productflavors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        tvGreeting.text = "Greetings from Paid version"
+    }
+}
